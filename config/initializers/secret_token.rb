fe8e5bc9c0fd3afabebc76cd015011d9dde9c826
Kiyoto Shinomura <kyt@shinomura.org>
2014-01-25 18:34:50 +0900

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestMongo::Application.config.secret_key_base = '28bc50fe1d9b3579b56a303255e1fd43c6b621803043e5ea0914291af096995461f239bf9cc33f0e79e26e1c391a559e600acdb4f0ab912bf05e6e6aec11f572'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestMongo::Application.config.session_store :cookie_store, key: '_test_mongo_session'
